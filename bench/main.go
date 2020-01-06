e350356525ab7f5ce5485c53fe5084ec5a783699
Paul Baker <paul.nelson.baker@gmail.com>
2020-01-06 11:53:29 -0700

    Benchmarking code to measure my performance

--- bench/main.go
+++ bench/main.go
@@ -2,35 +2,77 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/paul-nelson-baker/concurrent-random/pool"
 	"github.com/paul-nelson-baker/concurrent-random/safe"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
 
 func main() {
-	steps := 100
+	benchmarkResults := map[string][]bench{}
+
+	steps := 30
 	baseSize := 1000
 	stepSize := 1000
+
+	var simpleBench []bench
 	for i := 0; i < steps; i++ {
-		pooledConcurrentRandom := createPooledConcurrentRandom(2)
-		b := benchmark(steps, baseSize+(i*stepSize), pooledConcurrentRandom)
-		log.Printf("Pooled: %+v", b)
-		b = benchmark(steps, baseSize+(i*stepSize), simpleConcurrentRandom)
-		log.Printf("Simple: %+v", b)
+		b := benchmark(steps, baseSize+(i*stepSize), simpleConcurrentRandom)
+		simpleBench = append(simpleBench, b)
+	}
+	benchmarkResults["simple_concurrency"] = simpleBench
+	poolSizes := []int{1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100,}
+	for _, poolSize := range poolSizes {
+		log.Printf("Pool Size: %d", poolSize)
+		pooledConcurrentRandom := createPooledConcurrentRandom(poolSize)
+		var pooledBench []bench
+		for i := 0; i < steps; i++ {
+			b := benchmark(steps, baseSize+(i*stepSize), pooledConcurrentRandom)
+			pooledBench = append(pooledBench, b)
+		}
+		name := fmt.Sprintf("pool-%d-concurrency", poolSize)
+		benchmarkResults[name] = pooledBench
+	}
+
+	if err := saveBenchmarks("concurrency-benchmark", benchmarkResults); err != nil {
+		log.Fatalln(err)
 	}
 }
 
-func benchmark(count, size int, action func(size int) int64) []int64 {
+func saveBenchmarks(filename string, benchmarks map[string][]bench) error {
+	if !strings.HasSuffix(filename, ".json") {
+		filename += ".json"
+	}
+	allBytes, err := json.Marshal(benchmarks)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, allBytes, 0644)
+}
+
+type bench struct {
+	Size  int     `json:"size"`
+	Times []int64 `json:"times"`
+}
+
+func benchmark(count, size int, action func(size int) int64) bench {
+	log.Printf("Count: %d, Size: %d", count, size)
 	var times = make([]int64, 0, count)
 	for i := 0; i < count; i++ {
 		millis := action(size)
 		times = append(times, millis)
 	}
-	return times
+	return bench{
+		Size:  size,
+		Times: times,
+	}
 }
 
 func simpleConcurrentRandom(size int) int64 {
