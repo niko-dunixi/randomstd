09dd94cc7780a417549b993e72e9bcf53b6c7d44
Paul Baker <paul.nelson.baker@gmail.com>
2020-01-22 11:30:20 -0700

    I've found that I was missing a couple of methods from the Rand type. I've organized the code to match the documentation, my hope is that methods aren't overlooked again.
    https://golang.org/pkg/math/rand/#Rand

--- random_go1.8.go
+++ random_go1.8.go
@@ -5,18 +5,20 @@
 package randomstd
 
 type Random interface {
-	Seed(seed int64)
+	ExpFloat64() float64
+	Float32() float32
+	Float64() float64
+	Int() int
+	Int31() int32
+	Int31n(n int32) int32
 	Int63() int64
+	Int63n(n int64) int64
+	Intn(n int) int
+	NormFloat64() float64
+	Perm(n int) []int
+	Read(p []byte) (n int, err error)
+	Seed(seed int64)
+	Shuffle(n int, swap func(i, j int))
 	Uint32() uint32
 	Uint64() uint64
-	Int31() int32
-	Int() int
-	Int63n(n int64) int64
-	Int31n(n int32) int32
-	Intn(n int) int
-	Float64() float64
-	Float32() float32
-	Perm(n int) []int
-	Shuffle(n int, swap func(i, j int))
-	Read(p []byte) (n int, err error)
 }
--- randpool/poolRandomFunctions.go
+++ randpool/poolRandomFunctions.go
@@ -4,78 +4,11 @@ import (
 	"github.com/paul-nelson-baker/randomstd"
 )
 
-func (p *pool) Seed(seed int64) {
-	panic("this is not supported on the random pool")
-}
-
-func (p *pool) Int63() int64 {
-	c := make(chan int64, 1)
-	defer close(c)
-	p.Work(func(r randomstd.Random) {
-		c <- r.Int63()
-	})
-	return <-c
-}
-
-func (p *pool) Uint32() uint32 {
-	c := make(chan uint32, 1)
-	defer close(c)
-	p.Work(func(r randomstd.Random) {
-		c <- r.Uint32()
-	})
-	return <-c
-}
-
-func (p *pool) Int31() int32 {
-	c := make(chan int32, 1)
-	defer close(c)
-	p.Work(func(r randomstd.Random) {
-		c <- r.Int31()
-	})
-	return <-c
-}
-
-func (p *pool) Int() int {
-	c := make(chan int, 1)
-	defer close(c)
-	p.Work(func(r randomstd.Random) {
-		c <- r.Int()
-	})
-	return <-c
-}
-
-func (p *pool) Int63n(n int64) int64 {
-	c := make(chan int64, 1)
-	defer close(c)
-	p.Work(func(r randomstd.Random) {
-		c <- r.Int63n(n)
-	})
-	return <-c
-}
-
-func (p *pool) Int31n(n int32) int32 {
-	c := make(chan int32, 1)
-	defer close(c)
-	p.Work(func(r randomstd.Random) {
-		c <- r.Int31n(n)
-	})
-	return <-c
-}
-
-func (p *pool) Intn(n int) int {
-	c := make(chan int, 1)
-	defer close(c)
-	p.Work(func(r randomstd.Random) {
-		c <- r.Intn(n)
-	})
-	return <-c
-}
-
-func (p *pool) Float64() float64 {
+func (p *pool) ExpFloat64() float64 {
 	c := make(chan float64, 1)
 	defer close(c)
 	p.Work(func(r randomstd.Random) {
-		c <- r.Float64()
+		c <- r.ExpFloat64()
 	})
 	return <-c
 }
@@ -89,6 +22,78 @@ func (p *pool) Float32() float32 {
 	return <-c
 }
 
+func (p *pool) Float64() float64 {
+	c := make(chan float64, 1)
+	defer close(c)
+	p.Work(func(r randomstd.Random) {
+		c <- r.Float64()
+	})
+	return <-c
+}
+
+func (p *pool) Int() int {
+	c := make(chan int, 1)
+	defer close(c)
+	p.Work(func(r randomstd.Random) {
+		c <- r.Int()
+	})
+	return <-c
+}
+
+func (p *pool) Int31() int32 {
+	c := make(chan int32, 1)
+	defer close(c)
+	p.Work(func(r randomstd.Random) {
+		c <- r.Int31()
+	})
+	return <-c
+}
+
+func (p *pool) Int31n(n int32) int32 {
+	c := make(chan int32, 1)
+	defer close(c)
+	p.Work(func(r randomstd.Random) {
+		c <- r.Int31n(n)
+	})
+	return <-c
+}
+
+func (p *pool) Int63() int64 {
+	c := make(chan int64, 1)
+	defer close(c)
+	p.Work(func(r randomstd.Random) {
+		c <- r.Int63()
+	})
+	return <-c
+}
+
+func (p *pool) Int63n(n int64) int64 {
+	c := make(chan int64, 1)
+	defer close(c)
+	p.Work(func(r randomstd.Random) {
+		c <- r.Int63n(n)
+	})
+	return <-c
+}
+
+func (p *pool) Intn(n int) int {
+	c := make(chan int, 1)
+	defer close(c)
+	p.Work(func(r randomstd.Random) {
+		c <- r.Intn(n)
+	})
+	return <-c
+}
+
+func (p *pool) NormFloat64() float64 {
+	c := make(chan float64, 1)
+	defer close(c)
+	p.Work(func(r randomstd.Random) {
+		c <- r.NormFloat64()
+	})
+	return <-c
+}
+
 func (p *pool) Perm(n int) []int {
 	c := make(chan []int, 1)
 	defer close(c)
@@ -96,17 +101,6 @@ func (p *pool) Perm(n int) []int {
 		c <- r.Perm(n)
 	})
 	return <-c
-}
-
-func (p *pool) Shuffle(n int, swap func(i, j int)) {
-	p.Work(func(r randomstd.Random) {
-		r.Shuffle(n, swap)
-	})
-}
-
-type readResult struct {
-	n   int
-	err error
 }
 
 func (p *pool) Read(pBytes []byte) (n int, err error) {
@@ -118,4 +112,28 @@ func (p *pool) Read(pBytes []byte) (n int, err error) {
 	})
 	result := <-c
 	return result.n, result.err
+}
+
+func (p *pool) Seed(seed int64) {
+	panic("this is not supported on the random pool")
+}
+
+func (p *pool) Shuffle(n int, swap func(i, j int)) {
+	p.Work(func(r randomstd.Random) {
+		r.Shuffle(n, swap)
+	})
+}
+
+func (p *pool) Uint32() uint32 {
+	c := make(chan uint32, 1)
+	defer close(c)
+	p.Work(func(r randomstd.Random) {
+		c <- r.Uint32()
+	})
+	return <-c
+}
+
+type readResult struct {
+	n   int
+	err error
 }
